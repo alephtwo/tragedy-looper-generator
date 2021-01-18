9332ea25b2f3126789c557fdb58f6ae96c91adde
Ben Christianson <thhuntertgm@gmail.com>
2021-01-18 15:41:07 -0600

    Add basic/first roles

---
+++ src/data/Roles.ts
@@ -0,0 +1,56 @@
+import { Role } from '../types/Role';
+
+export const Roles: Record<string, Role> = {
+  keyPerson: {
+    id: '95d137b0-8652-4576-ae64-a7dc34a5f3f4',
+    name: 'Key Person',
+  },
+  killer: {
+    id: '0fa54005-f68b-4fc1-9c44-3c8db0b4b367',
+    name: 'Killer',
+  },
+  brain: {
+    id: '00e756b6-652f-4bc7-a85c-76c3d018074b',
+    name: 'Brain',
+  },
+  cultist: {
+    id: 'c5796a2b-012f-4e63-9421-3821ebd96faa',
+    name: 'Cultist',
+  },
+  conspiracyTheorist: {
+    id: '87a2518e-9e79-4dae-ae5f-1e58bd785887',
+    name: 'Conspiracy Theorist',
+  },
+  serialKiller: {
+    id: '85a0c633-fdf1-463c-9971-9b45bdc0cfcf',
+    name: 'Serial Killer',
+  },
+  curmudgeon: {
+    id: '9bf6604e-f0e8-4807-bebf-63b7a3b838b3',
+    name: 'Curmudgeon',
+  },
+  friend: {
+    id: 'ef68b5d1-adf6-4935-a298-2434e240f859',
+    name: 'Friend',
+  },
+  timeTraveller: {
+    id: '7774f887-6c8f-483f-aa1c-fab32835f867',
+    name: 'Time Traveller',
+  },
+  lover: {
+    id: '82094e99-b591-4470-974a-782f4e1e752e',
+    name: 'Lover',
+  },
+  lovedOne: {
+    id: '2910bfd8-8a87-4541-98ab-c01980efba10',
+    name: 'Loved One',
+  },
+  factor: {
+    id: '9293559d-01db-47a5-84d1-e37430f714f5',
+    name: 'Factor',
+  },
+  witch: {
+    id: '011c23fd-15b1-435e-a08e-b91e4d18ba51',
+    name: 'Witch',
+  },
+};
--- src/types/Plot.d.ts
+++ src/types/Plot.d.ts
@@ -1,4 +1,7 @@
+import { Role } from './Role';
+
 export interface Plot {
   id: string;
   name: string;
+  roles: Array<Role>;
 }
