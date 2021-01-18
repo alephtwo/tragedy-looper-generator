21768f780522c203086465cf68c31ed7a5c50133
Ben Christianson <thhuntertgm@gmail.com>
2021-01-18 16:54:40 -0600

    Add Incidents

---
+++ src/data/Incidents.ts
@@ -0,0 +1,150 @@
+import { Incident } from '../types/Incident';
+
+export const Incidents: Record<string, Incident> = {
+  // First Steps
+  murder: {
+    id: '4b50bb64-da08-448b-81ad-ad7765cf0e7d',
+    name: 'Murder',
+  },
+  increasingUnease: {
+    id: 'b61caaa1-a961-4c10-af3e-122c1321a8ab',
+    name: 'Increasing Unease',
+  },
+  suicide: {
+    id: '71e53334-02be-44e2-b33e-2126739b6310',
+    name: 'Suicide',
+  },
+  hospitalIncident: {
+    id: '2d4300de-801e-4436-9df6-20f8e3cebc4c',
+    name: 'Hospital Incident',
+  },
+  farawayMurder: {
+    id: '6fa6c79b-10ad-4e1a-87b9-a016234ce5bc',
+    name: 'Faraway Murder',
+  },
+  missingPerson: {
+    id: 'cd323450-6486-47c8-997d-e3bfbb75469e',
+    name: 'Missing Person',
+  },
+  spreading: {
+    id: '573fcf57-fc62-4a2e-96a0-49862082ff53',
+    name: 'Spreading',
+  },
+  // Basic Tragedy
+  foulEvil: {
+    id: '6cfbbac5-14c1-4082-a651-2786aac17b17',
+    name: 'Foul Evil',
+  },
+  butterflyEffect: {
+    id: '02068221-5e2e-407e-ba77-e5dbe7f31704',
+    name: 'Butterfly Effect',
+  },
+  // Midnight Zone
+  conspiracies: {
+    id: '6a0b35d7-2d04-4112-80b2-4c0fccf93580',
+    name: 'Conspiracies',
+  },
+  uproar: {
+    id: 'c6faf464-6f7d-4028-b563-037b2c1364bf',
+    name: 'Uproar',
+  },
+  fakeIncident: {
+    id: 'ed52f5dc-0100-4394-869c-e5d08cced458',
+    name: 'Fake Incident',
+  },
+  breakthrough: {
+    id: '40558603-fb7c-4bb8-9abd-1900c66f391b',
+    name: 'Breakthrough',
+  },
+  fakedSuicide: {
+    id: 'b23a6923-b3da-49ae-acab-95bd9529c345',
+    name: 'Faked Suicide',
+  },
+  confession: {
+    id: '6623e61f-8de9-4018-a14f-4b9ab7353f3a',
+    name: 'Confession',
+  },
+  // Mystery Circle
+  portent: {
+    id: '94060d4e-35c8-4623-94e9-a3c8521c73d8',
+    name: 'Portent',
+  },
+  terrorism: {
+    id: 'fc992060-f708-429a-9c20-6df09648e908',
+    name: 'Terrorism',
+  },
+  bestialMurder: {
+    id: '3394cc8e-3c63-4c51-8312-696323d7bd5a',
+    name: 'Bestial Murder',
+  },
+  aSuspiciousLetter: {
+    id: '96651be0-dd7c-40e2-805c-a1598500f17f',
+    name: 'A Suspicious Letter',
+  },
+  closedCircle: {
+    id: '6979e69f-c9a7-4178-8a92-26a2948aabad',
+    name: 'Closed Circle',
+  },
+  theSilverBullet: {
+    id: '6d8cc913-c92f-40d8-9bc9-6a386f05621e',
+    name: 'The Silver Bullet',
+  },
+  // Prime Evil
+  sacrilegiousMurger: {
+    id: '89e4b54c-d76a-47e7-8894-c15b6771a5f5',
+    name: 'Sacrilegious Murder',
+  },
+  theExecutioner: {
+    id: 'bcfee304-9320-4cd2-aadc-92fe1f4576de',
+    name: 'The Executioner',
+  },
+  darkRumor: {
+    id: 'a04bd2be-c382-4675-8a3d-3cc567abb2f6',
+    name: 'Dark Rumor',
+  },
+  barricade: {
+    id: '99efead4-810f-467d-bbee-b38610b04e0c',
+    name: 'Barricade',
+  },
+  evilContamination: {
+    id: 'b0893845-d541-46bd-baff-0d7b22f48eb8',
+    name: 'Evil Contamination',
+  },
+  nightOfMadness: {
+    id: '02c5d388-0cb4-458e-ad67-03c132917056',
+    name: 'Night of Madness',
+  },
+  awakenedCurse: {
+    id: 'bbeb6df0-cdd9-4919-aa29-9defd3e1d603',
+    name: 'Awakened Curse',
+  },
+  evangeliumOfTheDead: {
+    id: '3d2e0649-dd64-483f-8535-c7b2bc4fe954',
+    name: 'Evangelium of the Dead',
+  },
+  fountainOfFilth: {
+    id: '444b1aa1-b4e4-4530-8d8d-858b088ef928',
+    name: 'Fountain of Filth',
+  },
+  // Cosmic Mythology
+  insaneMurder: {
+    id: '6dc9d495-6dcb-4aed-8e30-8c3aa0470984',
+    name: 'Insane Murder',
+  },
+  massSuicide: {
+    id: '64759797-4d00-46d2-b43e-1f3bff993a7e',
+    name: 'Mass Suicide',
+  },
+  fireOfDemise: {
+    id: '882c2aa2-3c56-4d9f-8810-3763c3eb6111',
+    name: 'Fire of Demise',
+  },
+  houndDogScent: {
+    id: '87d18693-1678-46ea-90bd-2b2fa807853b',
+    name: 'Hound Dog Scent',
+  },
+  discovery: {
+    id: 'dfdb1988-7d95-4553-94c4-1da003166cba',
+    name: 'Discovery',
+  },
+};
---
+++ src/types/Incident.d.ts
@@ -0,0 +1,4 @@
+export interface Incident {
+  id: string;
+  name: string;
+}
