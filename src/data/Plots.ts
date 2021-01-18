89297e9ecddce0d0b2d5ad0c7533140ab8ef2df9
Ben Christianson <thhuntertgm@gmail.com>
2021-01-18 12:59:08 -0600

    Data entry - every plot

--- src/data/Plots.ts
+++ src/data/Plots.ts
@@ -1,11 +1,12 @@
 import { Plot } from '../types/Plot';
 
 export const MainPlots: Record<string, Plot> = {
+  // Base Game
   murderPlan: {
-    name: 'Murder Plan',
+    name: 'Murder Plan (Basic)',
   },
   theSealedItem: {
-    name: 'The Sealed Item',
+    name: 'The Sealed Item (Basic)',
   },
   signWithMe: {
     name: 'Sign with me!',
@@ -22,20 +23,85 @@ export const MainPlots: Record<string, Plot> = {
   aPlaceToProtect: {
     name: 'A Place to Protect',
   },
+  // Midnight Zone
+  theSealedItem2: {
+    name: 'The Sealed Item (Midnight Zone)',
+  },
+  secretRecord: {
+    name: 'Secret Record',
+  },
+  maleConfrontation: {
+    name: 'Male Confrontation',
+  },
+  theDevilsHand: {
+    name: "The Devil's Hand",
+  },
+  fatedConnections: {
+    name: 'Fated Connections',
+  },
+  // Mystery Circle
+  murderPlan2: {
+    name: 'Murder Plan (Mystery Circle)',
+  },
+  aQuiltOfIncidents: {
+    name: 'A Quilt of Incidents',
+  },
+  tightropePlan: {
+    name: 'Tightrope Plan',
+  },
+  theBlackSchool: {
+    name: 'The Black School',
+  },
+  aDropOfStrychnine: {
+    name: 'A Drop of Strychnine',
+  },
+  // Prime Evil
+  aNobleBloodline: {
+    name: 'A Noble Bloodline',
+  },
+  moonlightBeast: {
+    name: 'Moonlight Beast',
+  },
+  nightMistNightmare: {
+    name: 'Night Mist Nightmare',
+  },
+  theOnesFromTheGrave: {
+    name: 'The Ones from the Grave',
+  },
+  theCursedLand: {
+    name: 'The Cursed Land',
+  },
+  // Cosmic Mythology
+  choirToTheOutsideGod: {
+    name: 'Choir to the Outside God',
+  },
+  theSacredWordsOfDagon: {
+    name: 'The Sacred Words of Dagon',
+  },
+  theKingInYellow: {
+    name: 'The King in Yellow',
+  },
+  giantTimeBombAgain: {
+    name: 'Giant Time Bomb Again',
+  },
+  bloodyRites: {
+    name: 'Bloody Rites',
+  },
 };
 
 export const Subplots: Record<string, Plot> = {
+  // Base Game
   circleOfFriends: {
     name: 'Circle of Friends',
   },
   aLoveAffair: {
-    name: 'A Love Affair',
+    name: 'A Love Affair (Basic)',
   },
   theHiddenFreak: {
-    name: 'The Hidden Freak',
+    name: 'The Hidden Freak (Basic)',
   },
   anUnsettlingRumor: {
-    name: 'An Unsettling Rumor',
+    name: 'An Unsettling Rumor (Basic)',
   },
   paranoiaVirus: {
     name: 'Paranoia Virus',
@@ -51,5 +117,93 @@ export const Subplots: Record<string, Plot> = {
   },
   aHideousScript: {
     name: 'A Hideous Script',
+  },
+  // Midnight Zone
+  loveHateSpiral: {
+    name: 'Love-Hate Spiral',
+  },
+  witchesTeaTime: {
+    name: 'Witches Tea Time',
+  },
+  diceOfTheGods: {
+    name: 'Dice of the Gods',
+  },
+  unsafeTrigger: {
+    name: 'Unsafe Trigger',
+  },
+  showtimeOfDeath: {
+    name: 'Showtime of Death',
+  },
+  unansweredHeart: {
+    name: 'Unanswered Heart',
+  },
+  worshippersOfTheApocalypse: {
+    name: 'Worshippers of the Apocalypse',
+  },
+  // Mystery Circle
+  theHiddenFreak2: {
+    name: 'The Hidden Freak (Mystery Circle)',
+  },
+  isolatedInstitutionPsycho: {
+    name: 'Isolated Institution Psycho',
+  },
+  smellOfGunpowder: {
+    name: 'Smell of Gunpowder',
+  },
+  iAmAMasterDetective: {
+    name: 'I Am a Master Detective',
+  },
+  danceOfFools: {
+    name: 'Dance of Fools',
+  },
+  anAbsoluteWill: {
+    name: 'An Absolute Will',
+  },
+  trickyTwins: {
+    name: 'Tricky Twins',
+  },
+  // Prime Evil
+  thoseWithHabits: {
+    name: 'Those with Habits',
+  },
+  aLoveAffair2: {
+    name: 'A Love Affair (Prime Evil)',
+  },
+  witchsCurse: {
+    name: "Witch's Curse",
+  },
+  theKeyGirl: {
+    name: 'The Key Girl',
+  },
+  monsterIntrigue: {
+    name: 'Monster Intrigue',
+  },
+  panicAndObsession: {
+    name: 'Panic and Obsession',
+  },
+  peopleWhoDontListen: {
+    name: "People who don't Listen",
+  },
+  // Cosmic Evil
+  anUnsettlingRumor2: {
+    name: 'An Unsettling Rumor (Cosmic Evil)',
+  },
+  theResistance: {
+    name: 'The Resistance',
+  },
+  peopleWhoSaw: {
+    name: 'People Who Saw',
+  },
+  theProfoundRace: {
+    name: 'The Profound Race',
+  },
+  whispersFromTheDeep: {
+    name: 'Whispers from the Deep',
+  },
+  theFacelessGod: {
+    name: 'The Faceless God',
+  },
+  aTwistedTruth: {
+    name: 'A Twisted Truth',
   },
 };
