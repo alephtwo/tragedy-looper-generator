b766daa9f0abb9ec7f4a9fc1820cf9b44fd7c0bc
Ben Christianson <thhuntertgm@gmail.com>
2021-07-19 18:05:23 -0500

    Add culprit requirements to roles

--- src/types/data/Role.d.ts
+++ src/types/data/Role.d.ts
@@ -2,6 +2,7 @@ export interface Role {
   readonly id: string;
   readonly name: string;
   readonly unkillable: boolean;
+  readonly culprit: 'Never' | 'Optional' | 'Mandatory';
   readonly max?: number;
   readonly goodwillRefusal?: 'Optional' | 'Mandatory';
 }
