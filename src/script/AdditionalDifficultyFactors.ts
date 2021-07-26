9eb6a78649f828807c791b8bb334fb39f87bb754
Ben Christianson <thhuntertgm@gmail.com>
2021-07-26 13:25:05 -0500

    Add extra difficulty factors

--- src/script/AdditionalDifficultyFactors.ts
+++ src/script/AdditionalDifficultyFactors.ts
@@ -7,70 +7,93 @@ import { Script } from '../types/Script';
 
 export type DifficultyFactor = -1 | 0 | 1;
 
-export const AdditionalDifficultyFactors: AdditionalDifficultyFactorsDatabase = {
-  increaseIfGirlHasRole:
-    (role: Role) =>
-    (script: Script): DifficultyFactor => {
-      const met = script.cast.some((c) => c.character.descriptors.has('Girl') && c.role.id === role.id);
-      return met ? 1 : 0;
-    },
-  modifyIfCharacterHasRole:
-    (character: Character, role: Role, modify: -1 | 1) =>
-    (script: Script): DifficultyFactor => {
-      const met = script.cast.some((c) => c.character.id === character.id && c.role.id === role.id);
-      return met ? modify : 0;
-    },
-  increaseIfRoleIsCulprit:
-    (role: Role) =>
-    (script: Script): DifficultyFactor => {
-      const met = script.cast.some((c) => c.role.id === role.id && c.incidentTriggers.length > 0);
-      return met ? 1 : 0;
-    },
-  increaseIfCharacterIsCulprit:
-    (character: Character) =>
-    (script: Script): DifficultyFactor => {
-      const met = script.cast.some((c) => c.character.id === character.id && c.incidentTriggers.length > 0);
-      return met ? 1 : 0;
-    },
-  increaseIfIncidentPresent:
-    (incident: Incident) =>
-    (script: Script): DifficultyFactor => {
-      return determineIfIncidentIsPresent(script.cast, incident) ? 1 : 0;
-    },
-  increaseIfIncidentPresentWithRole:
-    (incident: Incident, role: Role) =>
-    (script: Script): DifficultyFactor => {
-      const incidentPresent = determineIfIncidentIsPresent(script.cast, incident);
-      const rolePresent = script.cast.some((c) => c.role.id === role.id);
+export const increaseIfGirlHasRole =
+  (role: Role) =>
+  (script: Script): DifficultyFactor => {
+    const met = script.cast.some((c) => c.character.descriptors.has('Girl') && c.role.id === role.id);
+    return met ? 1 : 0;
+  };
 
-      return incidentPresent && rolePresent ? 1 : 0;
-    },
-  increaseIfIncidentWithMainPlot:
-    (incident: Incident, plot: Plot) =>
-    (script: Script): DifficultyFactor => {
-      const mainPlotMatches = script.mainPlot.id === plot.id;
-      const incidentPresent = determineIfIncidentIsPresent(script.cast, incident);
+const modifyIfCharacterHasRole =
+  (modify: -1 | 1) =>
+  (character: Character, role: Role) =>
+  (script: Script): DifficultyFactor => {
+    const met = script.cast.some((c) => c.character.id === character.id && c.role.id === role.id);
+    return met ? modify : 0;
+  };
 
-      return mainPlotMatches && incidentPresent ? 1 : 0;
-    },
-  decreaseUnlessCharacterHasRole:
-    (character: Character, role: Role) =>
-    (script: Script): DifficultyFactor => {
-      const characterHasRole = script.cast.some((c) => c.role.id === role.id && c.character.id === character.id);
-      return characterHasRole ? 0 : -1;
-    },
-};
+export const increaseIfCharacterHasRole = modifyIfCharacterHasRole(1);
+export const decreaseIfCharacterHasRole = modifyIfCharacterHasRole(-1);
 
-interface AdditionalDifficultyFactorsDatabase {
-  increaseIfGirlHasRole: (role: Role) => (script: Script) => DifficultyFactor;
-  modifyIfCharacterHasRole: (character: Character, role: Role, modify: -1 | 1) => (script: Script) => DifficultyFactor;
-  increaseIfRoleIsCulprit: (role: Role) => (script: Script) => DifficultyFactor;
-  increaseIfCharacterIsCulprit: (character: Character) => (script: Script) => DifficultyFactor;
-  increaseIfIncidentPresent: (incident: Incident) => (script: Script) => DifficultyFactor;
-  increaseIfIncidentPresentWithRole: (incident: Incident, role: Role) => (script: Script) => DifficultyFactor;
-  increaseIfIncidentWithMainPlot: (incident: Incident, plot: Plot) => (script: Script) => DifficultyFactor;
-  decreaseUnlessCharacterHasRole: (character: Character, role: Role) => (script: Script) => DifficultyFactor;
-}
+export const increaseIfRoleIsCulprit =
+  (role: Role) =>
+  (script: Script): DifficultyFactor => {
+    const met = script.cast.some((c) => c.role.id === role.id && c.incidentTriggers.length > 0);
+    return met ? 1 : 0;
+  };
+
+export const increaseIfCharacterIsCulprit =
+  (character: Character) =>
+  (script: Script): DifficultyFactor => {
+    const met = script.cast.some((c) => c.character.id === character.id && c.incidentTriggers.length > 0);
+    return met ? 1 : 0;
+  };
+
+export const increaseIfIncidentPresent =
+  (incident: Incident) =>
+  (script: Script): DifficultyFactor => {
+    return determineIfIncidentIsPresent(script.cast, incident) ? 1 : 0;
+  };
+
+export const increaseIfIncidentPresentWithRole =
+  (incident: Incident, role: Role) =>
+  (script: Script): DifficultyFactor => {
+    const incidentPresent = determineIfIncidentIsPresent(script.cast, incident);
+    const rolePresent = script.cast.some((c) => c.role.id === role.id);
+
+    return incidentPresent && rolePresent ? 1 : 0;
+  };
+
+export const increaseIfIncidentWithMainPlot =
+  (incident: Incident, plot: Plot) =>
+  (script: Script): DifficultyFactor => {
+    const mainPlotMatches = script.mainPlot.id === plot.id;
+    const incidentPresent = determineIfIncidentIsPresent(script.cast, incident);
+
+    return mainPlotMatches && incidentPresent ? 1 : 0;
+  };
+
+export const decreaseUnlessCharacterHasRole =
+  (character: Character, role: Role) =>
+  (script: Script): DifficultyFactor => {
+    const characterHasRole = script.cast.some((c) => c.role.id === role.id && c.character.id === character.id);
+    return characterHasRole ? 0 : -1;
+  };
+
+export const increaseIfCharacterHasAnyGoodwillRefusal =
+  (character: Character) =>
+  (script: Script): DifficultyFactor => {
+    const matches = script.cast.some((c) => c.character.id === character.id && c.role.goodwillRefusal !== undefined);
+    return matches ? 1 : 0;
+  };
+
+export const increaseIfCharacterHasMandatoryGoodwillRefusal =
+  (character: Character) =>
+  (script: Script): DifficultyFactor => {
+    const matches = script.cast.some((c) => c.character.id === character.id && c.role.goodwillRefusal === 'Mandatory');
+    return matches ? 1 : 0;
+  };
+
+export const increaseIfCharacterHasAnyGoodwillRefusalWithIncient =
+  (character: Character, incident: Incident) =>
+  (script: Script): DifficultyFactor => {
+    const characterHasGoodwillRefusal = script.cast.some(
+      (c) => c.character.id === character.id && c.role.goodwillRefusal !== undefined
+    );
+    const incidentPresent = determineIfIncidentIsPresent(script.cast, incident);
+
+    return characterHasGoodwillRefusal && incidentPresent ? 1 : 0;
+  };
 
 function determineIfIncidentIsPresent(cast: Array<CastMember>, incident: Incident) {
   return cast.flatMap((c) => c.incidentTriggers).some((it) => it.incident.id === incident.id);
--- src/script/estimateLoops.tsx
+++ src/script/estimateLoops.tsx
@@ -4,7 +4,21 @@ import { Characters } from '../data/Characters';
 import { Roles } from '../data/Roles';
 import { Incidents } from '../data/Incidents';
 import { MainPlots } from '../data/Plots';
-import { AdditionalDifficultyFactors, DifficultyFactor } from './AdditionalDifficultyFactors';
+import {
+  decreaseIfCharacterHasRole,
+  decreaseUnlessCharacterHasRole,
+  DifficultyFactor,
+  increaseIfCharacterHasAnyGoodwillRefusal,
+  increaseIfCharacterHasAnyGoodwillRefusalWithIncient,
+  increaseIfCharacterHasMandatoryGoodwillRefusal,
+  increaseIfCharacterHasRole,
+  increaseIfCharacterIsCulprit,
+  increaseIfGirlHasRole,
+  increaseIfIncidentPresent,
+  increaseIfIncidentPresentWithRole,
+  increaseIfIncidentWithMainPlot,
+  increaseIfRoleIsCulprit,
+} from './AdditionalDifficultyFactors';
 
 export function estimateLoops(script: Script): number {
   const plots = [script.mainPlot].concat(script.subplots);
@@ -35,46 +49,50 @@ export function estimateLoops(script: Script): number {
 const additionalFactors: Array<(script: Script) => DifficultyFactor> = [
   // Things that increase difficulty
   // Setting a girl as a Key Person
-  AdditionalDifficultyFactors.increaseIfGirlHasRole(Roles.keyPerson),
+  increaseIfGirlHasRole(Roles.keyPerson),
   // Setting a girl as the Killer or the Loved One
-  AdditionalDifficultyFactors.increaseIfGirlHasRole(Roles.killer),
-  AdditionalDifficultyFactors.increaseIfGirlHasRole(Roles.lovedOne),
+  increaseIfGirlHasRole(Roles.killer),
+  increaseIfGirlHasRole(Roles.lovedOne),
   // Setting Godly Being or the Patient as the Witch.
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.godlyBeing, Roles.witch, 1),
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.patient, Roles.witch, 1),
+  increaseIfCharacterHasRole(Characters.godlyBeing, Roles.witch),
+  increaseIfCharacterHasRole(Characters.patient, Roles.witch),
   // Setting the Police Officer or the Patient as the Time Traveler.
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.policeOfficer, Roles.timeTraveller, 1),
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.patient, Roles.timeTraveller, 1),
+  increaseIfCharacterHasRole(Characters.policeOfficer, Roles.timeTraveller),
+  increaseIfCharacterHasRole(Characters.patient, Roles.timeTraveller),
   // Setting the Patient as a Friend.
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.patient, Roles.friend, 1),
+  increaseIfCharacterHasRole(Characters.patient, Roles.friend),
   // Having the culprit of an incident as the Conspiracy Theorist.
-  AdditionalDifficultyFactors.increaseIfRoleIsCulprit(Roles.conspiracyTheorist),
+  increaseIfRoleIsCulprit(Roles.conspiracyTheorist),
   // Incidents triggered by the Rich Man's Daughter or the Henchman.
-  AdditionalDifficultyFactors.increaseIfCharacterIsCulprit(Characters.richMansDaughter),
-  AdditionalDifficultyFactors.increaseIfCharacterIsCulprit(Characters.henchman),
+  increaseIfCharacterIsCulprit(Characters.richMansDaughter),
+  increaseIfCharacterIsCulprit(Characters.henchman),
   // Incidents triggered by the Lover or Loved One.
-  AdditionalDifficultyFactors.increaseIfRoleIsCulprit(Roles.lover),
-  AdditionalDifficultyFactors.increaseIfRoleIsCulprit(Roles.lovedOne),
+  increaseIfRoleIsCulprit(Roles.lover),
+  increaseIfRoleIsCulprit(Roles.lovedOne),
   // Hospital Incident, if you intend to trigger it. (Assume we do.)
-  AdditionalDifficultyFactors.increaseIfIncidentPresent(Incidents.hospitalIncident),
+  increaseIfIncidentPresent(Incidents.hospitalIncident),
   // Faraway Murder in scripts where you have a Key Person or a Friend.
-  AdditionalDifficultyFactors.increaseIfIncidentPresentWithRole(Incidents.farawayMurder, Roles.keyPerson),
-  AdditionalDifficultyFactors.increaseIfIncidentPresentWithRole(Incidents.farawayMurder, Roles.friend),
+  increaseIfIncidentPresentWithRole(Incidents.farawayMurder, Roles.keyPerson),
+  increaseIfIncidentPresentWithRole(Incidents.farawayMurder, Roles.friend),
   // Foul Evil when you have the Sealed Item as the main plot.
-  AdditionalDifficultyFactors.increaseIfIncidentWithMainPlot(Incidents.foulEvil, MainPlots.theSealedItem),
+  increaseIfIncidentWithMainPlot(Incidents.foulEvil, MainPlots.theSealedItem),
+  // Having Goodwill Refusal on the Shrine Maiden or Doctor
+  increaseIfCharacterHasAnyGoodwillRefusal(Characters.shrineMaiden),
+  increaseIfCharacterHasAnyGoodwillRefusal(Characters.doctor),
+  // Having Mandatory Goodwill Refusal on the Nurse
+  increaseIfCharacterHasMandatoryGoodwillRefusal(Characters.nurse),
+  // Spreading, when the Doctor has Goodwill Refusal.
+  increaseIfCharacterHasAnyGoodwillRefusalWithIncient(Characters.doctor, Incidents.spreading),
   // TODO: INCREASE: Setting the Boss as something that connects to the board (Conspiracy Theorist, Serial Killer)
-  // TODO: INCREASE: Having Goodwill Refusal on the Shrine Maiden or Doctor
-  // TODO: INCREASE: Having Mandatory Goodwill Refusal on the Nurse
   // TODO: INCREASE: Giving the Mystery Boy a role that exists in only 1 plot.
-  // TODO: INCREASE: Spreading, when the Doctor has Goodwill Refusal.
 
   // Things that decrease difficulty
   // Setting the Office Worker as anything other than a person.
-  AdditionalDifficultyFactors.decreaseUnlessCharacterHasRole(Characters.officeWorker, Roles.person),
+  decreaseUnlessCharacterHasRole(Characters.officeWorker, Roles.person),
   // Setting the Shrine Maiden, Pop Idol, or Boss as the Time Traveler.
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.shrineMaiden, Roles.timeTraveller, -1),
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.popIdol, Roles.timeTraveller, -1),
-  AdditionalDifficultyFactors.modifyIfCharacterHasRole(Characters.boss, Roles.timeTraveller, -1),
+  decreaseIfCharacterHasRole(Characters.shrineMaiden, Roles.timeTraveller),
+  decreaseIfCharacterHasRole(Characters.popIdol, Roles.timeTraveller),
+  decreaseIfCharacterHasRole(Characters.boss, Roles.timeTraveller),
   // TODO: DECREASE: Having the Godly Being as something directly connected to the loss conditions.
   // TODO: DECREASE: Having a character with a forbidden area as something that connects to the board.
 ];
