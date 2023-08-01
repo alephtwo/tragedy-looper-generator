2459acef5396b9f2e65f3c9900b916645b25cc35
Ben Christianson <thhuntertgm@gmail.com>
2023-08-01 14:35:43 -0500

    Add lang selector

--- src/view/Application.tsx
+++ src/view/Application.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect, useReducer } from "react";
+import { useEffect, useState, useReducer } from "react";
 import { useTranslation } from "react-i18next";
 import { ScriptGenerator } from "./ScriptGenerator";
 import { reducer, initialState } from "../logic/State";
@@ -7,13 +7,18 @@ import * as ScriptCard from "./ScriptCard";
 import { Cheatsheet } from "./Cheatsheet";
 
 export function Application(): JSX.Element {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [lang, setLang] = useState("en");
   const { script } = state;
 
   useEffect(() => {
     document.title = t("scaffolding.title");
   }, []);
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
 
   function ScriptInfo(): React.JSX.Element {
     if (script === null) {
@@ -31,6 +36,11 @@ export function Application(): JSX.Element {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col p-2 gap-2">
+        <div className="flex justify-end">
+          <select className="form-select rounded shadow" value={lang} onChange={(e) => setLang(e.target.value)}>
+            <option value="en">🇬🇧 EN</option>
+          </select>
+        </div>
         <ScriptGenerator state={state} dispatch={dispatch} />
         <ScriptInfo />
       </div>
