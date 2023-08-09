4bb7f82e6a3deb0c10ee1c8b4afb97eb2a97e021
Ben Christianson <thhuntertgm@gmail.com>
2023-08-09 09:12:52 -0500

    Further alter user experience

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,10 @@ import { CssBaseline, ThemeProvider, colors, createTheme } from "@mui/material";
 const defaultTheme = createTheme();
 const theme = createTheme({
   palette: {
-    primary: colors.red,
+    primary: colors.purple,
+    background: {
+      default: colors.deepPurple[500],
+    },
   },
   typography: {
     fontFamily: `"Noto Sans", ${defaultTheme.typography.fontFamily}`,
@@ -22,11 +25,30 @@ const theme = createTheme({
     },
   },
   components: {
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          borderColor: colors.grey[500],
+        },
+      },
+    },
     MuiTableCell: {
-      // Need to make it a little bit bolder
       styleOverrides: {
         head: {
+          // Need to make it a little bit bolder
           fontWeight: 700,
+        },
+        root: {
+          // // Need to make the table lines show up
+          borderBottomColor: colors.grey[500],
+        },
+      },
+    },
+    MuiSelect: {
+      // Don't want the background color to show
+      styleOverrides: {
+        outlined: {
+          backgroundColor: "white",
         },
       },
     },
