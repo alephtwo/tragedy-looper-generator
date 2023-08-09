f2e6600b9cdef3c9f2bba7a4a8106c2f9924439b
Ben Christianson <thhuntertgm@gmail.com>
2023-08-08 22:30:20 -0500

    Clean up generator panel

--- src/view/ScriptGenerator.tsx
+++ src/view/ScriptGenerator.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { TragedySetPicker } from "./TragedySetPicker";
 import { NumberPicker } from "./NumberPicker";
 import { Message, State } from "../logic/State";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Paper } from "@mui/material";
 
 interface ScriptGeneratorProps {
   state: State;
@@ -19,56 +19,58 @@ export function ScriptGenerator(props: ScriptGeneratorProps): React.JSX.Element 
   }
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <TragedySetPicker
-          id="tragedy-set"
-          selected={tragedySet}
-          onChange={(ts) => dispatch({ action: "set-tragedy-set", value: ts })}
-        />
+    <Paper sx={{ padding: 2 }} elevation={1}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={3}>
+          <TragedySetPicker
+            id="tragedy-set"
+            selected={tragedySet}
+            onChange={(ts) => dispatch({ action: "set-tragedy-set", value: ts })}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <NumberPicker
+            id="cast-size"
+            label={t("terms.castSize")}
+            min={6}
+            max={11}
+            value={castSize}
+            onChange={(n) => dispatch({ action: "set-cast-size", value: n })}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <NumberPicker
+            id="days"
+            label={t("terms.day", { count: 2 })}
+            min={4}
+            max={8}
+            value={days}
+            onChange={(n) => dispatch({ action: "set-days", value: n })}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <NumberPicker
+            id="incidents"
+            label={t("terms.incident", { count: 2 })}
+            min={0}
+            // NB: The inclusion of cast size here isn't strictly accurate.
+            // Serial Murder incidents can be perpetrated by the same cast member,
+            // which allows you to have a much smaller cast with more incidents.
+            // I don't want to fix that right now, so I'm just punting.
+            // It doesn't really make sense to fix it anyway in my opinion.
+            // If you're working on a script that goes against this, you should probably
+            // just be writing it yourself anyway.
+            max={Math.min(days, castSize, 7)}
+            value={incidents}
+            onChange={(n) => dispatch({ action: "set-incidents", value: n })}
+          />
+        </Grid>
+        <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <Button variant="contained" onClick={() => dispatch({ action: "generate" })}>
+            {t("scaffolding.generateScript")}
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <NumberPicker
-          id="cast-size"
-          label={t("terms.castSize")}
-          min={6}
-          max={11}
-          value={castSize}
-          onChange={(n) => dispatch({ action: "set-cast-size", value: n })}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <NumberPicker
-          id="days"
-          label={t("terms.day", { count: 2 })}
-          min={4}
-          max={8}
-          value={days}
-          onChange={(n) => dispatch({ action: "set-days", value: n })}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <NumberPicker
-          id="incidents"
-          label={t("terms.incident", { count: 2 })}
-          min={0}
-          // NB: The inclusion of cast size here isn't strictly accurate.
-          // Serial Murder incidents can be perpetrated by the same cast member,
-          // which allows you to have a much smaller cast with more incidents.
-          // I don't want to fix that right now, so I'm just punting.
-          // It doesn't really make sense to fix it anyway in my opinion.
-          // If you're working on a script that goes against this, you should probably
-          // just be writing it yourself anyway.
-          max={Math.min(days, castSize, 7)}
-          value={incidents}
-          onChange={(n) => dispatch({ action: "set-incidents", value: n })}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Button variant="contained" onClick={() => dispatch({ action: "generate" })}>
-          {t("scaffolding.generateScript")}
-        </Button>
-      </Grid>
-    </Grid>
+    </Paper>
   );
 }
