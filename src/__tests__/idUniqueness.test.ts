2267b161047a6536a2d26ea1d77036dd5c372a2a
Ben Christianson <thhuntertgm@gmail.com>
2024-04-04 00:46:12 -0500

    Switch to vitest

--- src/__tests__/idUniqueness.test.ts
+++ src/__tests__/idUniqueness.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect } from "vitest";
 
 import * as _ from "lodash";
 import { Characters } from "../data/Characters";
