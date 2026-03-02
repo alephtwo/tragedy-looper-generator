9a121f376dc7523cb7c668402d48cb9458ec6468
Ben Christianson <thhuntertgm@gmail.com>
2026-03-02 12:44:45 -0600

    feat: switch to markdownlint-cli2

---
+++ .markdownlint-cli2.mjs
@@ -0,0 +1,11 @@
+export default {
+  /**
+   * @type {import("markdownlint").Configuration}
+   */
+  config: {
+    "line-length": {
+      code_blocks: false,
+      tables: false,
+    },
+  },
+};
